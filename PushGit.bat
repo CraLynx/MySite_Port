9c3546c587ed7908427fe1c298bb690c190ac95f
CraLynx <roman-544@yandex.ru>
2017-10-12 11:49:23 +0300

    chage this file

--- PushGit.bat
+++ PushGit.bat
@@ -2,15 +2,16 @@
 cd /d d:\github\mysite_port
 git status
 :renamecom
-SET /P COM=Enter your comment:
-ECHO Your comment is - %COM%
-Echo Are you sure? Y/N
-SET /p Cheking= And your ansewer:
-if "%cheking%" == "Y" or "y" or "yes" or "YES"(
-git add -A
-git commit -m "%COM%"
-git push
-pause
-) else (
-goto renamecom
-)
+	SET /P COM=Enter your comment:
+	ECHO Your comment is - %COM%
+	Echo Are you sure? y/n
+		SET /p cheking=And your answer:
+	if "%checking%" == "y" (
+		git add -A
+		git commit -m "%COM%"
+		git push
+		pause
+	) else (
+		goto renamecom
+		)
+Pause
