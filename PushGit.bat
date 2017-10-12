ae8c5da12bbdf54f04b86bf92f78a23fa578b848
CraLynx <roman-544@yandex.ru>
2017-10-12 11:17:24 +0300

    Change border-color

--- PushGit.bat
+++ PushGit.bat
@@ -6,11 +6,10 @@ SET /P COM=Enter your comment:
 ECHO Your comment is - %COM%
 Echo Are you sure? Y/N
 SET /p Cheking= And your ansewer:
-if "%cheking%" == "Y" (
+if "%cheking%" == "Y" or "y" or "yes" or "YES"(
 git add -A
 git commit -m "%COM%"
 git push
-pause
 ) else (
 goto renamecom
 )
