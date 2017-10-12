adefc0dd62fb3de0a2bcfb2e66872e2d5ccc376b
CraLynx <roman-544@yandex.ru>
2017-10-12 11:09:42 +0300

    modif .bat

--- PushGit.bat
+++ PushGit.bat
@@ -1,10 +1,17 @@
 @echo off
 cd /d d:\github\mysite_port
 git status
+:renamecom
 SET /P COM=Enter your comment:
 ECHO Your comment is - %COM%
-PAUSE
+Echo Are you sure? Y/N
+SET /p Cheking= And your ansewer:
+if "%cheking%" == "Y" (
 git add -A
 git commit -m "%COM%"
 git push
 pause
+) else (
+goto renamecom
+)
+PAUSE
